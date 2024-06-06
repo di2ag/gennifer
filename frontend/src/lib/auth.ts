51927354191e2f8f839465e4d9e4ed68fe4dde99
SoniaBendre <soniabendre02@gmail.com>
2024-06-06 17:02:10 -0400

    refactor

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -4,9 +4,10 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import jwt_decode from "jwt-decode"
 import { collapseClasses } from "@mui/material";
 import { url } from "inspector";
-import { signOut } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import { error } from "console";
 import { JWT } from "next-auth/jwt";
+import Providers from "next-auth/providers";
 
 function getGoogleCredentials() {
     const clientId = process.env.GOOGLE_CLIENT_ID
@@ -48,6 +49,15 @@ function getUserDetails() {
     return url;
 }
 
+function getDjangoToken() {
+    const url = process.env.DJANGO_TOKEN_URL
+    if (!url || url.length === 0) {
+        throw new Error("Missing Django Token URL.");
+    }
+    return url;
+
+}
+
 async function getNewAccessAndRefreshTokens(token: JWT) {
     try {
 
@@ -59,21 +69,21 @@ async function getNewAccessAndRefreshTokens(token: JWT) {
         urlencoded.append("client_secret", getGenniferCredentials().clientSecret);
 
         const response = await fetch(getCredentials(), {
-            headers: { 
-                "Content-Type": "application/x-www-form-urlencoded", 
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
             },
             body: urlencoded,
             method: "POST",
         })
 
         const newToken = await response.json();
-        newToken.received_at = Date.now()/1000;
+        newToken.received_at = Date.now() / 1000;
         newToken.expires_at = newToken.expires_in + newToken.received_at;
-        
+
         // Request user details using newToken
         const userResponse = await fetch(getUserDetails(), {
             method: "GET",
-            headers: { 
+            headers: {
                 "Authorization": "Bearer " + newToken.access_token,
             },
         });
@@ -85,11 +95,11 @@ async function getNewAccessAndRefreshTokens(token: JWT) {
             ...newToken,
             ...user,
         }
-            } catch (error) {
-                console.error(error);
-                return { ...token, error: "RefreshAccessTokenError" };
-            }
-  }
+    } catch (error) {
+        console.error(error);
+        return { ...token, error: "RefreshAccessTokenError" };
+    }
+}
 
 
 export const authOptions: NextAuthOptions = {
@@ -100,10 +110,31 @@ export const authOptions: NextAuthOptions = {
     //     signIn: "/login",
     // },
     providers: [
-        // GoogleProvider({
-        //     clientId: getGoogleCredentials().clientId,
-        //     clientSecret: getGoogleCredentials().clientSecret,
-        // }),
+        {
+            id: "orcid",
+            name: "ORCID",
+            type: "oauth",
+            wellKnown: "https://orcid.org/.well-known/openid-configuration",
+            clientId: process.env.ORCID_CLIENT_ID,
+            clientSecret: process.env.ORCID_CLIENT_SECRET,
+            authorization: { params: { scope: "openid" } },
+            idToken: true,
+            checks: ["pkce", "state"],
+            profile(profile) {
+                return {
+                    id: profile.sub,
+                    id_token: profile.id_token,
+                    orcid_user: profile.user,
+                    refresh_token: profile.refresh_token,
+                    issued_at: profile.issued_at,
+                    expires_at: profile.expires_at,
+                    username: profile.username,
+                    access_token: profile.access_token,
+                    expires_in: profile.expires_in,
+                }
+            },
+        },
+
         CredentialsProvider({
             name: "credentials",
             id: "credentials",
@@ -112,8 +143,8 @@ export const authOptions: NextAuthOptions = {
                 password: { label: "Password", type: "password" },
             },
             async authorize(credentials, req) {
-                let username:string = credentials?.username!;
-                let password:string = credentials?.password!;
+                let username: string = credentials?.username!;
+                let password: string = credentials?.password!;
                 var urlencoded = new URLSearchParams();
                 urlencoded.append("grant_type", "password");
                 urlencoded.append("username", username);
@@ -121,22 +152,22 @@ export const authOptions: NextAuthOptions = {
 
                 const response = await fetch(getCredentials(), {
                     method: "POST",
-                    headers: { 
+                    headers: {
                         "Content-Type": "application/x-www-form-urlencoded",
-                        "Authorization": "Basic " + Buffer.from(getGenniferCredentials().clientId + ":" + getGenniferCredentials().clientSecret).toString("base64"), 
+                        "Authorization": "Basic " + Buffer.from(getGenniferCredentials().clientId + ":" + getGenniferCredentials().clientSecret).toString("base64"),
                     },
                     body: urlencoded,
                 });
-                
+
 
                 const token = await response.json();
-                token.received_at = Date.now()/1000;
-                token.expires_at = token.received_at + token.expires_in ;
+                token.received_at = Date.now() / 1000;
+                token.expires_at = token.received_at + token.expires_in;
 
                 // Request user details
                 const userResponse = await fetch(getUserDetails(), {
                     method: "GET",
-                    headers: { 
+                    headers: {
                         "Authorization": "Bearer " + token.access_token,
                     },
                 });
@@ -155,27 +186,55 @@ export const authOptions: NextAuthOptions = {
         })
     ],
     callbacks: {
-        async redirect({ url, baseUrl}) {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (account?.provider === "orcid") {
+                console.log("ORCID user signed in: ", user);
+
+                const { id, id_token } = user;
+
+                var urlencoded = new URLSearchParams();
+                urlencoded.append("user", id);
+                urlencoded.append("acces_token", id_token);
+
+                const djangoUserResponse = await fetch(getDjangoToken(), {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    body: urlencoded,
+                });
+
+                if (djangoUserResponse.ok) {
+                    const djangoUser = await djangoUserResponse.json();
+                    const { django_access_token, django_refresh_token } = djangoUser;
+                    user.access_token = django_access_token;
+                    user.refresh_token = django_refresh_token;
+                    return true
+                }
+            }
+            return false;
+          },
+        async redirect({ url, baseUrl }) {
             if (url.startsWith("/")) return `${baseUrl}${url}`
             else if (new URL(url).origin === baseUrl) return url
             return baseUrl
         },
-        
-        async jwt({token, user}) {
-            if(user) { // user is being signed in
+
+        async jwt({ token, user }) {
+            if (user) { // user is being signed in
                 return {
                     ...token,
                     ...user,
                 };
-            } else if ( Math.floor(token.expires_at - Date.now()/1000) > 0 ) { // token has not expired
+            } else if (Math.floor(token.expires_at - Date.now() / 1000) > 0) { // token has not expired
                 return token
             } else { // token has expired
                 return getNewAccessAndRefreshTokens(token);
             }
-        }, 
+        },
 
-        async session({session, token}) {
-            if(token) {
+        async session({ session, token }) {
+            if (token) {
                 session.user.access = token.access;
                 session.user.refresh = token.refresh;
                 session.user.access_token = token.access_token;
@@ -192,7 +251,7 @@ export const authOptions: NextAuthOptions = {
             session.error = token.error;
             return session;
         },
- 
-        
+
+
     },
 }
